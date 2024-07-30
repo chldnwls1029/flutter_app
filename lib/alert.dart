b9bbee729fcd8777bcfe46878e57308bd78b1e10
603-23\kccistc <wi885038@gmail.com>
2024-07-30 11:11:22 +0900

    Reapply "alert.dart"
    This reverts commit 8182a06260b2abe40b395515674ec5d810a814a8.

---
+++ lib/alert.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  get builder => null;
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("alert 입니다"),
+      ),
+      body: Container(
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext con) {
+                    return AlertDialog(
+                      title: const Text("Dialog Title"),
+                      content: Container(
+                        child: const Text("Dialog Content"),
+                      ),
+                      actions: [
+                        TextButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: const Text("Close"))
+                      ],
+                    );
+                  });
+            },
+            child: const Text("팝업 버튼"),
+          ),
+        ),
+      ),
+    );
+  }
+}
